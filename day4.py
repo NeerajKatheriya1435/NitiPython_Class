c887d25abea9322580434a57d3b260a9e6118f65
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-06-27 16:11:23 +0530

    operators

---
+++ day4.py
@@ -0,0 +1,52 @@
+a=8
+b=7
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+ 
+#  7 -- 8
+# 7.78--> ciel value=8  floor value=7
+
+# 5 -- 6
+# 5.45--> ciel value=6  floor value=5
+
+# 18 -- 19
+# 18.09--> ciel value=19  floor value=18
+
+#  
+# 34.897--> ciel value=35  floor value=34
+# 8.9678
+
+# print(8//3)
+# print(15//4)
+
+# print(28%3)
+# print(45%12)
+
+# print(2**6)
+# comparison operator
+# print(5==8)  # F
+# print(5==5) # T
+# print(5!=5) # F
+# print(5>=5) # T
+# print(5<=5) # T
+# print(5<=45) # T 
+# print(6<=5) # F
+# print(5<5) # F
+# print(5>5) # F
+
+num1=4
+# nitiVar+=5
+# nitiVar=nitiVar+5  #9
+# nitiVar-=7  #nitivar=nitivar-7
+
+# num1*=6 #24
+
+# num1/=4
+# num1//=3
+# num1%=5
+num1**=3
+
+print(num1)
+
