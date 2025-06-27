ea60dc8c4ed96b9a4d000da81940cb824aa3dada
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-06-27 16:51:16 +0530

    String Intro

---
+++ day5.py
@@ -0,0 +1,40 @@
+# paise>500
+
+a=9
+b=6
+
+# print(a>8 and b>8 and a<b)
+# print(a<8 or b>8)
+# print(not (a>b))
+# name="Niti"
+# sirName="Tiwari"
+# lastName="Singh"
+# print(name+" "+sirName)
+# print("hello Ayush and "+name)
+# print("hello ayush and",name)
+
+# print(name,sirName,lastName,sep=" and ",end="Rohit")
+# print(name+sirName+lastName)
+# print(name,sirName,lastName,sep=",,,",end="End of Doc")
+# """Hello tiwari"""
+# print("hello Kmar  \nsanu kaise ho")
+# print("""hello Kmar 
+#       hello guys how are you 
+#       sanu kaise ho""")
+
+
+str1="Shivam How"
+
+# print(str1[4])
+# print(str1[6])
+
+# for character in str1:
+#     print(character)
+
+var1=int(input("Enter the side: "))
+
+areaSquare=var1*var1
+perimeterSquare=4*var1
+
+print("The area is:",areaSquare)
+print("The perimeter is:",perimeterSquare)
