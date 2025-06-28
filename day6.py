f966caed9e8051020fe3db08747b648ba32f3773
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-06-28 17:56:48 +0530

    some string methods

---
+++ day6.py
@@ -0,0 +1,45 @@
+# yourName="Niti Tiwari"
+# yourName="014345678910"
+# list1=[7,9,67]
+# list2=(7,9,6,7,5)
+# print(yourName)
+# print(len(yourName))
+# print(len(list1))
+# print(len(list2))
+# print(yourName[3:7])
+# print(yourName[2:9])
+
+# N i  t  i     T  i  w  a  r  i 
+# 0 1  2  3     4  5  6  7  8  9 
+#10 -9 -8 -7  sp -6  -5 -4 -3 -2 -1
+ 
+# p ri nt (yourName[:6])
+# print(yourName[-7:-1])
+# print(yourName[-1:-8])
+# print(yourName[2:11:4])
+
+# str1="        You Are good boy         " 
+# str1="      Ayush      "
+# str1="Ayush !!!!!!!!"
+# print(str1)
+# print(str1.upper())
+# print(str1.lower())
+# print(str1)
+# print(str1.strip())
+# print(str1.rstrip("!"))
+# print(str1.lstrip())
+
+# str2="Ay,ush,Kumar Si ngh"
+# yourName="Niti TiwariSanjnaRamesh Shivam"
+# print(str2.replace("Ayush","Shiva"))
+# print(str2.replace("Ayu","Shiva"))
+# yourName="i am not SO Fine"
+# print(yourName.capitalize())
+
+
+# str1 = "Welcome to the Console!!!"
+# print(str1.center(80))
+
+yourName="NiTi TiwaTi"
+print(yourName.count("Ti"))
+
