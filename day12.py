48d0f54f5ffed2f39db62b49d90c7941908cb993
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-07-03 17:55:35 +0530

    List and methods

---
+++ day12.py
@@ -0,0 +1,37 @@
+nitiList=[3,5,7,"Shivam",8,6,5]
+# print(nitiList)
+# print(len(nitiList))
+# print(nitiList[0])
+# print(nitiList[1])
+# print(nitiList[2:6:2])
+# print(nitiList[-3])
+# print(nitiList[45])
+
+# for itemNiti in nitiList:
+#     print(itemNiti)
+
+# names = ["Milo", "Sarah", "Bruno", "Anastasia", "Rosa"]
+# namesWith_O = [item for item in names if len(item)>4]
+# print(namesWith_O)
+
+# list1=[8,4,4,5,3,6,4]
+
+# list1.sort(reverse=True)
+# list1.sort()
+# list1.reverse()
+
+# print(list1)
+# print(list1.index(14))
+# print(list1.count(4))
+
+# nitiList=list1.copy()
+# print(nitiList)
+# list1.append("kumar")
+# print(list1)
+# list1.insert(2,"Kumar")
+# print(list1)
+list1=[8,4,4,5,3,6,4]
+color=["Red","Yellow","Green"]
+# list1.extend(color)
+listNitya=list1+color
+print(listNitya)
--- del1.py
+++ del1.py
@@ -9,35 +9,59 @@
 # # newBoy.game="PubG"
 # newBoy.showDetails()
 
-class Human:
-    def __init__(self,name,game):
-        print(self.__dir__())
-        self.name=name
-        self.game=game
-        print(self.__dir__())
+# class Human:
+#     def __init__(self,name,game):
+#         print(self.__dir__())
+#         self.name=name
+#         self.game=game
+#         print(self.__dir__())
 
-    @property
-    def getName(self):
-        return self.name
+#     @property
+#     def getName(self):
+#         return self.name
     
 
-    @getName.setter
-    def getName(self,newName):
-        if isinstance(newName, str):
-            self.name=newName
-        else:
-            print("Hello")
+#     @getName.setter
+#     def getName(self,newName):
+#         if isinstance(newName, str):
+#             self.name=newName
+#         else:
+#             print("Hello")
         
     
-    def showDetails(self):
-        print(f"Hello {self.name} and my game is {self.game}")
+#     def showDetails(self):
+#         print(f"Hello {self.name} and my game is {self.game}")
 
-newBoy1=Human("Neeraj","Hill Climb")
+# newBoy1=Human("Neeraj","Hill Climb")
 # newBoy2=Human("suman","Manik Babu")
 # newBoy1.name="Manik"
 # newBoy1.game="PubG"
 # newBoy1.showDetails()
 # newBoy2.showDetails()
-newBoy1.getName="123"
-print(newBoy1.getName)
+# newBoy1.getName="123"
+# print(newBoy1.getName)
 # print()
+
+# class Employee:
+#   def __init__(self, name):
+#     self.name = name
+#   def show(self):
+#     print(f"The name is {self.name}")
+
+# class Dancer:
+#   def __init__(self, dance):
+#     self.dance = dance
+
+#   def show(self):
+#     print(f"The dance is {self.dance}")
+
+# class DancerEmployee(Employee, Dancer):
+#   def __init__(self, dance, name):
+#     self.dance = dance
+#     self.name = name
+
+# o  = DancerEmployee("Kathak", "Shivani")
+# print(o.name)
+# print(o.dance)
+# o.show() 
+# print(DancerEmployee.mro())
