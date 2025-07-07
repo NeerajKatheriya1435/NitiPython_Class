a15493e811a55eb667fe81c4a655fcba5b92c029
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-07-07 16:03:15 +0530

    Tuple Explained and methods

---
+++ day13.py
@@ -0,0 +1,20 @@
+tup1=(4,5,8,9,8,6,5,0,6)
+# print(tup1,type(tup1))
+# print(tup1[2])
+# print(tup1[-2])
+
+# if 15 in tup1:
+#     print("Yes")
+# else:
+#     print("No")
+# print(tup1[-7:-2])
+
+# list1=list(tup1)
+# list1.append(997)
+# tup1= tuple(list1)
+# print(tup1)
+
+# for item in tup1:
+#     print(item)
+# print(tup1.count(8))
+# print(tup1.index(8,3))
--- pattern.py
+++ pattern.py
@@ -66,13 +66,20 @@ input=5
 #         last=last-1
 #     print("\n")
 
-n=6
-m=1
-for i in range(1,n):
-    for j in range(1,n-i):
-        print(" ",end=" ")
-    for k in range(1,m+1):
-        # print(k)
-        print("*",end=" ")
-    m=m+2
-    print("\n")
+# n=9
+# for i in range(n,0,-1):
+#     for j in range(1,i):
+#         print(" ",end=" ")
+#     for k in range(1,n+2-i):
+#         print("*",end=" ")
+#     print()
+
+# n=9 # how many times outer loop will work
+# m=2 # priting star pattern
+# for i in range(n,0,-1):
+#     for i in range(1,i):
+#         print(" ",end=" ")
+#     for k in range(1,m):
+#         print("*",end=" ")
+#     m=m+2
+#     print()
