33f5eb804c120967809492e89a26222ee2f8a56e
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-07-11 15:08:20 +0530

    error and shot hand if else

---
+++ day15.py
@@ -0,0 +1,40 @@
+# dict1={"name":"Shubham","age":45,"college":"GOVT Polytechnic"}
+# print(dict1)
+# print(dict1["age"])
+
+# print(dict1.items())
+
+# for key,value in dict1.items():
+
+#     print(f"The key is {key} and value is {value}")
+
+# for key in dict1.keys():
+#     print(key)
+# for key in dict1.keys():
+#     print(dict1[key])
+
+# for key1 in dict1.values():
+#     print(key1)
+
+# dict1["niti"]="kumari"
+# dict1["animal"]="dog"
+# dict1["mutable"]="yes"
+# print(dict1)
+# dict1.pop("age")
+# dict1.popitem()
+# del dict1["niti"]
+# dict1.clear()
+# print(dict1)
+# dict1={"name":"Shubham","age":45,"college":"GOVT Polytechnic"}
+# dict2={"gram":"Behati","Range":2000}
+# # print(dict1.get("mutable"))
+
+# dict1.update(dict2)
+# print(dict1)
+
+# dict2={}
+# list1=["subham","john","peter"]
+# newDict=dict2.fromkeys(list1,65)
+
+# newDict=dict1.setdefault("name1")
+# print(newDict)
---
+++ day16.py
@@ -0,0 +1,31 @@
+# print("Hello niti")
+
+# n=int(input("Enter the number less than 6: "))
+# if(n<6):
+#     print("The number is less than 6")
+# else:
+#     raise ValueError("Eror Genrated!!!")
+
+# lis1=[4,6,9]
+# index=int(input("Enter the index number: "))
+# Totalindex=len(lis1)
+# if(index>=Totalindex):
+#     raise IndexError("False Index Found")
+# else:
+#     print(lis1[index])
+
+age=24
+# if age>30:
+#     print("Age is Greater than 30")
+# else:
+#     print("Age is less than 30")
+
+# print("Age is greter than 30") if age>30 else print("Age is less than 30")
+# status = "Adult" if age >= 18 else "Minor"
+# print(status)
+
+# listNew=["Shubham","Neeraj","Shivani"]
+listNew="StringMy"
+
+for index, value in enumerate(listNew):
+    print(f"My index is {index} and value is {value}")
