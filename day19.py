8f9748bcf6e49a2ba5e4d2130ec8d176f4d95201
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-07-13 13:48:38 +0530

    os module in python

---
+++ day19.py
@@ -0,0 +1,25 @@
+import os
+# print(os.getcwd())
+# os.mkdir("NitiFolder")
+# os.makedirs("NitiFolder/Child/Niti")
+# for i in range(1,101):
+#     os.makedirs(f"Niticlasses/Day {i}")
+
+# print(os.getcwd())
+# os.chdir("../")
+# print(os.getcwd())
+# files = os.listdir()
+# print(files)
+
+# for i in range(1,22):
+#     os.rename(f"Niticlasses/Day {i}", f"Niticlasses/Day {i+100}")
+# for i in range(1,22):
+    # os.rename(f"Niticlasses/Day {i}", f"Niticlasses/Day {i+100}")
+    # os.removedirs(f"Niticlasses/Day {i+100}")
+
+# os.rmdir("niti")
+
+if os.path.exists("data.txt"):
+    print("File exists")
+else:
+    print("Not found")
